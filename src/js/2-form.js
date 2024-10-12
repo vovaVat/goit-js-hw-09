a218229b98bd831bf106ba51269edb4bda45ec72
Volodymyr <vovavat97@gmail.com>
2024-10-12 21:09:07 +0300

    hz

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const form= document.querySelector(".feedback-form");
+let formData={
+  email:"",
+  message:""
+}
+
+loadFormDataFromStorage();
+
+form.addEventListener('input', onInputForm);
+form.addEventListener('submit', onSubmitForm)
+
+function onInputForm(event){
+  const {name,value}= event.target;
+  formData[name]=value.trim();
+
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+}
+
+function onSubmitForm(event){
+  event.preventDefault();
+
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData = { email: "", message: "" };
+  form.reset();
+}
+
+function loadFormDataFromStorage() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+}
