53c24471362b669babe4e44d5ed86d482c4af5c1
Volodymyr <vovavat97@gmail.com>
2024-10-20 20:08:05 +0300

    hz

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,30 +1,31 @@
-const form= document.querySelector(".feedback-form");
-let formData={
-  email:"",
-  message:""
-}
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector("input[name='email']");
+const messageInput = form.querySelector("textarea[name='message']");
+
+let formData = {
+  email: "",
+  message: ""
+};
 
 loadFormDataFromStorage();
 
 form.addEventListener('input', onInputForm);
-form.addEventListener('submit', onSubmitForm)
+form.addEventListener('submit', onSubmitForm);
 
-function onInputForm(event){
-  const {name,value}= event.target;
-  formData[name]=value.trim();
+function onInputForm(event) {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
 
-  localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
 
-function onSubmitForm(event){
+function onSubmitForm(event) {
   event.preventDefault();
-
 
   if (!formData.email || !formData.message) {
     alert('Fill please all fields');
     return;
   }
-
 
   console.log(formData);
 
